1bea589dae156e25371eaf92576b403063d8c692
Jowita Osinga <jowita.osinga@gmail.com>
2020-01-31 11:13:51 +0100

    second test function

--- tests/test_flaskapp.py
+++ tests/test_flaskapp.py
@@ -1,4 +1,5 @@
-import unittest,json
+import unittest
+import json
 
 from main import app
 
@@ -19,7 +20,15 @@ class FlaskTestCase(unittest.TestCase):
         self.assertEqual(resp['answer'],35,'Multiply endpoint failed known answer 7*5 = 35')
 
     # TODO DEFINE TWO MORE TESTS ON THE END POINTS
+    def test_touppercase(self):
+        response = self.app.get('/touppercase?s=hello')
+        resp = response.data.decode()
+        self.assertEqual(resp, 'HELLO', 'Failed to get uppercase of hello')
 
+    def test_app(self):
+        response = self.app.get('/')
+        resp = response.data.decode()
+        self.assertEqual(resp, 'Hello World!', 'Failed to return hello world')
 
 if __name__ == '__main__':
     unittest.main()
